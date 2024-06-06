4404845fd046f59756f3e6fea1b336c398d9c64b
daviyaic <124395832+daviyaic@users.noreply.github.com>
2024-06-06 09:11:00 -0300

    Update rpa_python.py

--- rpa_python.py
+++ rpa_python.py
@@ -1,4 +1,5 @@
 from time import sleep
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
@@ -9,8 +10,8 @@ driver = webdriver.Chrome(options=options)
 driver.get("https://br.investing.com/currencies/usd-brl")
  
 sleep(8)
-dolar = driver.find_element(By.CLASS_NAME, "text-5xl/9 font-bold text-[#232526] md:text-[42px] md:leading-[60px]").text
-tempo = driver.find_element(By.TAG_NAME, 'time').text
+dolar = driver.find_element(By.CSS_SELECTOR, "#__next > div.md\:relative.md\:bg-white > div.relative.flex > div.grid.flex-1.grid-cols-1.px-4.pt-5.font-sans-v2.text-\[\#232526\].antialiased.xl\:container.sm\:px-6.md\:grid-cols-\[1fr_72px\].md\:gap-6.md\:px-7.md\:pt-10.md2\:grid-cols-\[1fr_420px\].md2\:gap-8.md2\:px-8.xl\:mx-auto.xl\:gap-10.xl\:px-10 > div.min-w-0 > div.flex.flex-col.gap-6.md\:gap-0 > div.flex.gap-6 > div.flex-1 > div.mb-3.flex.flex-wrap.items-center.gap-x-4.gap-y-2.md\:mb-0\.5.md\:gap-6 > div.text-5xl\/9.font-bold.text-\[\#232526\].md\:text-\[42px\].md\:leading-\[60px\]").text
+tempo = driver.find_element(By.CSS_SELECTOR, '#__next > div.md\:relative.md\:bg-white > div.relative.flex > div.grid.flex-1.grid-cols-1.px-4.pt-5.font-sans-v2.text-\[\#232526\].antialiased.xl\:container.sm\:px-6.md\:grid-cols-\[1fr_72px\].md\:gap-6.md\:px-7.md\:pt-10.md2\:grid-cols-\[1fr_420px\].md2\:gap-8.md2\:px-8.xl\:mx-auto.xl\:gap-10.xl\:px-10 > div.min-w-0 > div.flex.flex-col.gap-6.md\:gap-0 > div.flex.gap-6 > div.flex-1 > div.flex.items-center.gap-2 > div.flex.items-center.gap-1.text-xs\/4.text-\[\#5B616E\] > time').text
 driver.close()
 dolar = dolar.replace(',','.')
 
@@ -22,7 +23,7 @@ from datetime import datetime
 data_atual = datetime.now()
 data_atual = data_atual.strftime("%Y-%m-%d")
 
-horario = tempo.split()[3]
+horario = tempo
 
 print(data_atual)
 print(horario)
